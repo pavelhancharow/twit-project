8424a39f5286e2e9b826429ede36b4e83adc56c0
Pavel <pl.hancharou@gmail.com>
2021-01-29 16:42:05 +0300

    feat: initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import AppHeader from './components/AppHeader';
+import PostAddForm from './components/PostAddForm';
+import PostList from './components/PostList';
+import PostStatusFilter from './components/PostStatusFilter';
+import SearchPanel from './components/SearchPanel';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <AppHeader />
+      <div className="search-panel d-flex">
+        <SearchPanel />
+        <PostStatusFilter />
+      </div>
+      <PostList />
+      <PostAddForm />
     </div>
   );
 }
---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,12 @@
+import './app-header.css';
+
+const AppHeader = () => {
+  return (
+    <header className="app-header d-flex">
+      <h1>Pavel Hancharou</h1>
+      <h2>3 записи, из них 0 понравилось</h2>
+    </header>
+  )
+}
+
+export default AppHeader
